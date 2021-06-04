46a86af6ec317903045502a5c0118fef22e29abf
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-06-04 12:23:33 +0900

    socket install, chat setting

--- node/app.js
+++ node/app.js
@@ -16,13 +16,23 @@ const listen = require('socket.io');
 const io = listen(server);
 
 io.on('connection', (socket) => { 
+    socket.on('server message', function(data){
+    $('#chatLog').append('<li> '+ data.username +' : ' + data.message + ' </li>');
+ });
+
+socket.on('join', function(data){
+    $('#chatLog').append('<li> ' + data.username + '님이 방을 들어왔습니다 </li>');
+});
+
+socket.on('leave', function(data){
+    $('#chatLog').append('<li> ' + data.username + '님이 방을 나갔습니다 </li>');
+});
+
     //console.log("소켓 작동");
     socket.on('client message', (data) => {
-        //전체 메세지 뿌려줌
-        console.log(data.message);
-    //     io.emit('server message', {
-    //         message : data.message
-    //     });
+        io.emit('server message', {
+            message : data.message
+        });
     });
 
 });
