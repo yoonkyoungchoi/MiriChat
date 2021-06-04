772bd044d1733be83166785bcb49b1facedb6939
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-06-04 14:17:41 +0900

    docker 설정

--- node/app.js
+++ node/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
-
+const redis = require('socket.io-redis');
 const app = express();
 const port = 3000;
-
 
 app.get('/', ( _ ,res) => {
     res.sendFile(__dirname + '/index.html');
@@ -14,25 +13,32 @@ const server = app.listen( port, () => {
 
 const listen = require('socket.io');
 const io = listen(server);
+io.adapter(redis({ host: 'redis', port: 6379 }));
+
+const color = [
+    "yellow",
+    "green",
+    "red",
+    "blue",
+    "white",
+    "black",
+]
 
 io.on('connection', (socket) => { 
-    socket.on('server message', function(data){
-    $('#chatLog').append('<li> '+ data.username +' : ' + data.message + ' </li>');
- });
 
-socket.on('join', function(data){
-    $('#chatLog').append('<li> ' + data.username + '님이 방을 들어왔습니다 </li>');
-});
+    const username = color[ Math.floor(Math.random() * 6) ];
 
-socket.on('leave', function(data){
-    $('#chatLog').append('<li> ' + data.username + '님이 방을 나갔습니다 </li>');
-});
+    socket.broadcast.emit( 'join',  {  username  } );
 
-    //console.log("소켓 작동");
     socket.on('client message', (data) => {
         io.emit('server message', {
+            username ,
             message : data.message
         });
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave', { username });
     });
 
 });
